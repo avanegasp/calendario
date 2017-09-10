d5fac90403980d41bd283f8e64f4721bcdc6970d
avanegasp <arco527@gmail.com>
2017-09-09 20:20:58 -0500

    Colors ans title... all ready

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 $('.col').click(function(){
-  var color = 'rgb(255,0,0)';
+  var r = Math.floor(Math.random() * 255)
+  var g = Math.floor(Math.random() * 255)
+  var b = Math.floor(Math.random() * 255)
+
+  var color = 'rgb(' + r + ', ' + g + ', ' + b + ')';
   $(this).css('background', color);
 })
